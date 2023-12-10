36ceffa5a3864cbb6fd3945896cadee8861c0e37
gjaubert <ggjaubert@gmail.com>
2023-12-10 11:32:27 +0100

    added cli completed

--- src/cli.go
+++ src/cli.go
@@ -2,11 +2,26 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"os"
+	"strconv"
 )
 
 type CLI struct {
 	bc *Blockchain
+}
+
+func (cli *CLI) printUsage() {
+	fmt.Println("Usage:")
+	fmt.Println("  addblock -data BLOCK_DATA - add a block to the blockchain")
+	fmt.Println("  printchain - print all the blocks of the blockchain")
+}
+
+func (cli *CLI) validateArgs() {
+	if len(os.Args) < 2 {
+		cli.printUsage()
+		os.Exit(1)
+	}
 }
 
 func (cli *CLI) Run() {
@@ -20,8 +35,14 @@ func (cli *CLI) Run() {
 	switch os.Args[1] {
 	case "addblock":
 		err := addBlockCmd.Parse(os.Args[2:])
+		if err != nil {
+			fmt.Println("Error " + err.Error())
+		}
 	case "printchair":
 		err := printChainCmd.Parse(os.Args[2:])
+		if err != nil {
+			fmt.Println("Error " + err.Error())
+		}
 	default:
 		cli.printUsage()
 		os.Exit(1)
@@ -37,5 +58,29 @@ func (cli *CLI) Run() {
 
 	if printChainCmd.Parsed() {
 		cli.printChain()
+	}
+}
+
+func (cli *CLI) addBlock(data string) {
+	cli.bc.AddBlock(data)
+	fmt.Println("Succes!")
+}
+
+func (cli *CLI) printChain() {
+	bci := cli.bc.Iterator()
+
+	for {
+		block := bci.Next()
+
+		fmt.Printf("Prev. hash: %x\n", block.PrevBlockHash)
+		fmt.Printf("Data: %s\n", block.Data)
+		fmt.Printf("Hash: %x\n", block.Hash)
+		pow := NewProofOfWork(block)
+		fmt.Printf("PoW: %s\n", strconv.FormatBool(pow.Validate()))
+		fmt.Println()
+
+		if len(block.PrevBlockHash) == 0 {
+			break
+		}
 	}
 }
